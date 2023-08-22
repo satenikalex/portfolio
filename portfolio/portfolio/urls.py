cdfb8284d93f04dfba59aecd7bfe41f35a40b08b
satenikalex <alexaniansatenik@gmail.com>
2023-08-22 10:07:56 +0400

    portf

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,10 +19,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = (
+    [
     path('admin/', admin.site.urls),
     path("", include('resume.urls')),
-
+]
 + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+)
